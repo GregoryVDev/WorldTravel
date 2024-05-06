d10ebfa422da049c1c264579dbca84dc811cc447
GregoryVDev <arexo@hotmail.fr>
2024-05-06 15:01:00 +0200

    Edited the burger menu and added the button login

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ menuBurger.addEventListener("click", () => {
   nav.classList.toggle("active");
   menuBurger.classList.toggle("active");
 });
+
+// const nav = document.getElementById("nav");
+// const menuBurger = document.getElementById("burger");
+
+// menuBurger.addEventListener("click", () => {
+//   // Toggle the 'active' class on the navigation menu and menu burger icon
+//   nav.classList.toggle("active");
+//   menuBurger.classList.toggle("active");
+
+//   // Toggle the body's overflow property to prevent scrolling when the menu is open
+//   if (nav.classList.contains("active")) {
+//     document.body.style.overflow = "hidden";
+//   } else {
+//     document.body.style.overflow = "auto";
+//   }
+// });
