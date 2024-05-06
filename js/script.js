11a61366a285e4059ef138be13cef150ba15e734
GregoryVDev <arexo@hotmail.fr>
2024-05-06 11:27:42 +0200

    test burger menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const nav = document.getElementById("nav");
+const menuBurger = document.getElementById("burger");
+
+menuBurger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  menuBurger.classList.toggle("active");
+});
